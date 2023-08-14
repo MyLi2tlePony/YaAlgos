30d0d1f959cc9907ab6bf0f8e8e9a01709878ad9
MyLi2tlePony <shabandrew@mail.ru>
2023-08-14 18:48:36 +0300

    2

---
+++ 2/solution.go
@@ -0,0 +1,15 @@
+package main
+
+func sort(arr []int) {
+	for i := range arr {
+		minIndex := i
+
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+}
